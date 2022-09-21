3c8a335b992026226402c97a237c10621e1c45a1
Dominik Macko <xmacko1@fi.muni.cz>
2022-09-21 18:17:00 +0200

    WIP: sync utils

--- folder_sync/utils.py
+++ folder_sync/utils.py
@@ -1,25 +1,41 @@
 # TODO description and documentation
 
 import hashlib
+import os
+from typing import Iterable
 
-def delete_file(): 
-    pass
+# https://stackoverflow.com/questions/33135038/how-do-i-use-os-scandir-to-return-direntry-objects-recursively-on-a-directory
+def scantree(dir_path: str) -> Iterable[os.DirEntry]:
 
-def copy_file():
-    pass
+    with os.scandir(dir_path) as dir_it:
+        for entry in dir_it:
+            if entry.is_dir(follow_symlinks=False):
+                yield from scantree(entry.path)  # see below for Python 2.x
+            else:
+                yield entry
 
-def create_dir(): 
-    pass
+def build_path_with_diff_dir(input_path: str, input_dir_path: str, target_dir_path: str) -> str:
 
-def file_content_matches():
-    pass
+    path = build_path_without_prefix(input_path, input_dir_path)
+    return os.path.join(target_dir_path, path)
 
-def get_file_md5_hash(file_path: str, buf_size: int=65536) -> str:
+def build_path_without_prefix(path: str, prefix_path: str) -> str:
+
+    return os.path.relpath(path, prefix_path)
+
+def file_content_matches(file_path: str, file_path2: str):
+    
+    return get_file_md5_hash(file_path) == get_file_md5_hash(file_path2)
+
+# https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+def get_file_md5_hash(file_path: str) -> str:
     
     md5 = hashlib.md5()
-    while True:
-        data = f.read(buf_size)
-        if not data:
-            break
-        md5.update(data)
+    with open(file_path, "rb") as f:
+        while True:
+            chunk = f.read(md5.block_size)
+            if not chunk:
+                break
+            md5.update(chunk)
     return md5.hexdigest()
+
